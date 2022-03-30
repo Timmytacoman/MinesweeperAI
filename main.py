627cbdd707e63ddfc1ff17bf249a8e8e80006c5f
Timmytacoman <72770854+Timmytacoman@users.noreply.github.com>
2022-03-29 19:32:04 -0600

    Added Tile class

--- main.py
+++ main.py
@@ -2,41 +2,76 @@ import numpy
 import pygame
 import argparse
 
-# default board size
-defaultRows = 10
-defaultCols = 10
+# default board edge size
+defaultEdgeSize = 20
 
 # instantiate the parser
 parser = argparse.ArgumentParser(description='Specify game parameters')
 
 # add arguments for rows and cols
-parser.add_argument('--rows', default=defaultRows, help="enter number of rows to be generated")
-parser.add_argument('--cols', default=defaultCols, help="enter number of columns to be generated")
+# TODO add screensize argument
+parser.add_argument('--size', type=int, default=defaultEdgeSize, help="enter the size of the board to generate")
 
-# collect input rows and cols
+# collect input size
 args = parser.parse_args()
-rows = args.rows
-cols = args.cols
+size = args.size
 
-print(f"Generating board size: {rows} x {cols}")
-exit()
+# screen dimensions
+screenSize = size * 30
+
+print(f"Generating board size: {size}")
+print(f"Screen size: {screenSize}")
 
 # init the pygame library
 pygame.init()
 
-# setup the grid
-grid = numpy.zeros((rows, cols))
+# determine tile length
+tileLength = screenSize / size
+print(f"tileLength = {tileLength}")
 
 # Set up the drawing window
-screen = pygame.display.set_mode([750, 750])
+screen = pygame.display.set_mode([screenSize, screenSize])
+
+# global list of all the tile objects
+listOfTiles = []
 
 
-def draw():
-    # draw tiles
+class Tile:
+    def __init__(self, x, y, color):
+        # x and y denote the top left position of the tile
+        self.x = x
+        self.y = y
+        self.color = color
 
-    pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(30, 30, 60, 60))
+    def __str__(self):
+        # this is called when you print a Tile object
+        return str(f"x={self.x}\ny={self.y}\ncolor={self.color}")
+
+
+def init_board():
+    # initialize the board
+
+    # fill the screen white
+    screen.fill((255, 255, 255))
+
+    # construct all the tiles
+    for row in range(size):
+        for col in range(size):
+            # construct tile obj
+            x = col * tileLength
+            y = row * tileLength
+            color = (100, 100, 100)
+            print(Tile(x, y, color))
+
+            # pygame.rect obj takes (left, top, width, height)
+            # pygame.draw.rect(screen, tileBorderColor,
+            #                  pygame.Rect(col * tileLength, row * tileLength, tileLength, tileLength), 2)
+
     pygame.display.flip()
 
+
+# initialize the board
+init_board()
 
 # Run until the user asks to quit
 running = True
@@ -47,13 +82,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+        # on mouse click
+        if event.type == pygame.MOUSEBUTTONUP:
+            # declare position
+            xClick, yClick = pygame.mouse.get_pos()
 
-    draw()
-
-    # Flip the display
-    pygame.display.flip()
+            print(xClick, yClick)
 
 # Done! Time to quit.
 print("Goodbye!")
