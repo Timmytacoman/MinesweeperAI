2fea6c8f801ac1e2bdeabecb7e676aa6c629da10
Timmytacoman <72770854+Timmytacoman@users.noreply.github.com>
2022-03-29 22:39:37 -0600

    Rename vars

--- main.py
+++ main.py
@@ -3,27 +3,27 @@ import argparse
 import random
 
 # default board edge size
-defaultEdgeSize = 20
+default_edge_size = 20
 
 # bomb percentage
-bombPercentage = 20
-numBombs = 0
+bomb_percentage = 20
+num_bombs = 0
 
 # instantiate the parser
 parser = argparse.ArgumentParser(description='Specify game parameters')
 
 # add arguments for rows and cols
-parser.add_argument('--size', type=int, default=defaultEdgeSize, help="enter the size of the board to generate")
+parser.add_argument('--size', type=int, default=default_edge_size, help="enter the size of the board to generate")
 
 # collect input size
 args = parser.parse_args()
 size = args.size
 
 # screen dimensions
-screenSize = size * 30
+screen_size = size * 30
 
 print(f"Generating board size: {size}")
-print(f"Screen size: {screenSize}")
+print(f"Screen size: {screen_size}")
 
 # init the pygame library
 pygame.init()
@@ -32,40 +32,41 @@ pygame.init()
 pygame.display.set_caption('Minesweeper AI')
 
 # determine tile length
-tileLength = screenSize / size
-print(f"tileLength = {tileLength}")
+tile_length = screen_size / size
+print(f"tileLength = {tile_length}")
 
 # Set up the drawing window
-screen = pygame.display.set_mode([screenSize, screenSize])
+screen = pygame.display.set_mode([screen_size, screen_size])
 
 # global list of all the tile objects
-listOfTiles = []
+list_of_tiles = []
+
 
 
 def getScaledImage(name):
     # returns the scaled image of the desired image
-    return pygame.transform.scale(pygame.image.load(f"./assets/{name}.png"), (tileLength, tileLength))
+    return pygame.transform.scale(pygame.image.load(f"./assets/{name}.png"), (tile_length, tile_length))
 
 
 # image constructing
-tileImage = getScaledImage('tile')
-emptyImage = getScaledImage('empty')
-bombImage = getScaledImage('bomb')
+tile_image = getScaledImage('tile')
+empty_image = getScaledImage('empty')
+bomb_image = getScaledImage('bomb')
 
 
 class Tile:
-    def __init__(self, x, y, isBomb, image=None):
+    def __init__(self, x, y, is_bomb, image=None):
         # x and y denote the top left position of the tile
         self.x = x
         self.y = y
-        self.isBomb = isBomb
+        self.is_bomb = is_bomb
         self.image = image
 
     def __str__(self):
         # this is called when you print a Tile object
-        return str(f"x={self.x}\ny={self.y}\nisBomb={self.isBomb}")
+        return str(f"x={self.x}\ny={self.y}\nisBomb={self.is_bomb}")
 
-    def setImage(self, image):
+    def set_image(self, image):
         self.image = image
 
 
@@ -76,53 +77,53 @@ def init_board():
     for row in range(size):
         for col in range(size):
             # construct tile obj
-            x = col * tileLength
-            y = row * tileLength
+            x = col * tile_length
+            y = row * tile_length
 
             # randomize bombs
-            isBomb = False
-            randNum = random.randint(1, 100)
-            if 1 <= randNum <= bombPercentage:
-                isBomb = True
-                global numBombs
-                numBombs += 1
+            is_bomb = False
+            rand_num = random.randint(1, 100)
+            if 1 <= rand_num <= bomb_percentage:
+                is_bomb = True
+                global num_bombs
+                num_bombs += 1
             # create tile
-            newTile = Tile(x, y, isBomb, tileImage)
+            new_tile = Tile(x, y, is_bomb, tile_image)
             # append it to list
-            listOfTiles.append(newTile)
+            list_of_tiles.append(new_tile)
             # draw tiles
-            screen.blit(tileImage, (x, y))
+            screen.blit(tile_image, (x, y))
 
     pygame.display.flip()
 
 
 def draw_board():
-    for tile in listOfTiles:
+    for tile in list_of_tiles:
         screen.blit(tile.image, (tile.x, tile.y))
     pygame.display.flip()
 
 
 def on_click(x, y):
     clicked_tile = None
-    shortest = tileLength
+    shortest = tile_length
 
     # determine what tile was closest
-    for tile in listOfTiles:
+    for tile in list_of_tiles:
         # distance formula
-        xDistance = abs(tile.x + (tileLength / 2) - x)
-        yDistance = abs(tile.y + (tileLength / 2) - y)
-        distance = xDistance + yDistance
+        x_distance = abs(tile.x + (tile_length / 2) - x)
+        y_distance = abs(tile.y + (tile_length / 2) - y)
+        distance = x_distance + y_distance
         if distance < shortest:
             shortest = distance
             clicked_tile = tile
 
     # if the clicked tile was a bomb
-    if clicked_tile.isBomb:
-        clicked_tile.setImage(bombImage)
+    if clicked_tile.is_bomb:
+        clicked_tile.set_image(bomb_image)
         return clicked_tile
 
     # set new image
-    clicked_tile.setImage(emptyImage)
+    clicked_tile.set_image(empty_image)
     return clicked_tile
 
 
@@ -144,8 +145,8 @@ while running:
         # on mouse click
         if event.type == pygame.MOUSEBUTTONUP:
             # declare position
-            xClick, yClick = pygame.mouse.get_pos()
-            print(on_click(xClick, yClick))
+            x_click, y_click = pygame.mouse.get_pos()
+            print(on_click(x_click, y_click))
 
 # Done! Time to quit.
 print("Goodbye!")
